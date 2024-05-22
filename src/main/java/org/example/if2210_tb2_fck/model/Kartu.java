ef0098641aa97ad5d587d543871ffa87f8e5506f
Thea Josephine <theajosephine04@gmail.com>
2024-05-22 20:59:47 +0700

    feat(shuffle initialize): DeckInventory

--- src/main/java/org/example/if2210_tb2_fck/model/Kartu.java
+++ src/main/java/org/example/if2210_tb2_fck/model/Kartu.java
@@ -4,6 +4,7 @@ package org.example.if2210_tb2_fck.model;
 public class Kartu {
     private String nama;
     private boolean diLadang;
+    private String jenis;
 
     public Kartu(){
         this.nama = null;
@@ -14,8 +15,31 @@ public class Kartu {
         this.nama = name;
         this.diLadang = false;
     }
-
+    
     public String getName() {
         return this.nama;
+    }
+
+    public String getIcon(){
+        String basePath = "D:/IF2210_TB2_FCK/src/main/resources/org/example/if2210_tb2_fck/Assets/";
+        String subPath = "";
+        if (jenis.equals("Hewan")) {
+            subPath = "Hewan/";
+        } else if (jenis.equals("Produk")) {
+            subPath = "Produk/"; 
+        } else if (jenis.equals("Tanaman")) {
+            subPath = "Tanaman/";
+        } else if (jenis.equals("Item")) {
+            subPath = "Item/";
+        }
+        return basePath + subPath + this.nama + ".png";
+    }
+
+    public boolean getDiLadang(){
+        return this.diLadang;
+    }
+
+    public String getJenis(){
+        return this.jenis;
     }
 }
