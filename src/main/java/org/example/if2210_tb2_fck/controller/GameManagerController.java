532eeb950c08d51c9c1bc3f9e2bd91d8678b447e
novelxv <novelyaputriramadhani11@gmail.com>
2024-05-23 16:31:17 +0700

    feat(controller): add GameManagerController

---
+++ src/main/java/org/example/if2210_tb2_fck/controller/GameManagerController.java
@@ -0,0 +1,104 @@
+package org.example.if2210_tb2_fck.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.util.Random;
+
+//import org.example.if2210_tb2_fck.model.GameManager;
+import org.example.if2210_tb2_fck.model.Player;
+import org.example.if2210_tb2_fck.model.Toko;
+
+public class GameManagerController {
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private Button startButton;
+
+//    private GameManager game_manager;
+    private Player player1;
+    private Player player2;
+    private int current_turn;
+    private static final int MAX_TURNS = 20;
+    private Toko toko;
+
+    public GameManagerController(){
+        this.player1 = new Player("Player 1");
+        this.player2 = new Player("Player 2");
+        this.current_turn = 1;
+        this.toko = Toko.getInstance(0); // gatau
+    }
+
+    @FXML
+    public void initialize(){
+//        nextTurnButton.setOnAction(event -> handleNextTurn());
+    }
+
+    public void handleStartButton() throws IOException {
+        startTurn();
+    }
+
+    private void shuffleCards() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/if2210_tb2_fck/shuffle.fxml"));
+        Pane shufflePane = loader.load();
+        ShuffleController shuffleController = loader.getController();
+        shuffleController.setPlayer(getCurrentPlayer());
+        shuffleController.handleShuffle();
+        mainPane.getChildren().setAll(shufflePane);
+    }
+
+    public Player getCurrentPlayer(){
+        return current_turn % 2 == 1 ? player1 : player2;
+    }
+
+    public Player getOtherPlayer(){
+        return current_turn % 2 == 1 ? player2 : player1;
+    }
+
+    public void nextTurn() throws IOException {
+        current_turn++;
+        if (current_turn > MAX_TURNS){
+            endGame();
+        } else {
+            startTurn();
+        }
+    }
+
+    public int getCurrentTurn(){
+        return current_turn;
+    }
+
+    private void endGame(){
+        System.out.println("Game Over!");
+        // to-implement
+    }
+
+    public void startTurn() throws IOException {
+        shuffleCards();
+        if (bearAttackOccurs()){
+            bearAttack();
+        }
+        freeAction();
+    }
+
+    private boolean bearAttackOccurs(){
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+    private void bearAttack(){
+        // to-implement
+        System.out.println("Bear attack!");
+    }
+
+    private void freeAction(){
+        // to-implement
+        System.out.println("Free action phase...");
+    }
+}
